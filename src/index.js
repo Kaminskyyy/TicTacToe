65814e665316d8a6953c80b3561c4eddb7f8e39d
Kaminskyyy <87198204+Kaminskyyy@users.noreply.github.com>
2022-04-18 19:14:38 +0300

    Added port as environment variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { server } = require('./app.js');
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
 	console.log('Server is up on port ' + port);
